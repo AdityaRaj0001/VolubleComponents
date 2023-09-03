ff0de8f211b75896f29a3aa445418eb92d20faf1
AdityaRaj0001 <meadityaraj0001@gmail.com>
2023-09-03 17:58:15 +0530

    added dropdown toastify phone card toggler

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Toggler from "./components/Toggler"
+import Dropdown from "./components/Dropdown"
+import Notifications from "./components/Notifications"
+import Phone from "./components/Phone"
+import SquishyCard from "./components/SquishyCard"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Toggler></Toggler>
+      <Dropdown/>
+      <Notifications></Notifications>
+      <Phone/>
+      <SquishyCard/>
+    </>
+  )
+}
+
+export default App
